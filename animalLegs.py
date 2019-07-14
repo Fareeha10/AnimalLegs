9b4167eff081017f8216b3af8fc504d80ddfca66
Fareeha10 <42777932+Fareeha10@users.noreply.github.com>
2019-07-14 16:57:20 +0500

    Add files via upload

---
+++ animalLegs.py
@@ -0,0 +1,9 @@
+p=int(input("Enter pigs on your farm to calculate their legs:"))
+PIG=p*4
+ch=int(input("Enter chickens on your farm to calculate their legs:"))
+CHI=ch*2
+c=int(input("Enter cows on your farm to calculate their legs:"))
+COW=c*4
+s=PIG+CHI+COW
+print("TotaL number of animal legs on farm is:",s)
+
